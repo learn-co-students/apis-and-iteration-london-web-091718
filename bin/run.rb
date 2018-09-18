b5ac973105ca04e91625f4eda34150b9ea4af29f
Patrick <ctrlaltpat@gmail.com>
2018-09-18 16:21:46 +0100

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,10 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+# welcome
+# number = get_number_from_user
+# show_character_tv_series(number)
+
 welcome
-character = get_character_from_user
-show_character_movies(character)
+name= get_name_from_user
+show_character_info(name)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,34 +1,110 @@
+# require 'rest-client'
+# require 'json'
+# require 'pry'
+#
+# def get_character_by_name_from_api(name)
+#
+#   response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{name}")
+#   response_hash = JSON.parse(response_string)
+#
+# end
+#
+# def get_more_info(url)
+#   response_string = RestClient.get(url)
+#   response_hash = JSON.parse(response_string)
+# end
+#
+# def print_name(series_hash)
+#   puts "Name: #{series_hash["name"]}"
+#   # some iteration magic and puts out the movies in a nice list
+# end
+#
+# def print_series(series_hash)
+#   rtn_string = "Appeared in "
+#   series_hash["tvSeries"].each_with_index {
+#     |series,index|
+#     if index < series_hash["tvSeries"].count-1
+#       rtn_string << "#{series}, "
+#     else
+#       rtn_string << "and #{series}."
+#     end
+#   }
+#   puts rtn_string
+#   # some iteration magic and puts out the movies in a nice list
+# end
+#
+# def print_played_by(series_hash)
+#   puts "Played initially by #{series_hash["playedBy"][0]}."
+# end
+#
+# def print_books(series_hash)
+#   series_hash["books"].each do |book_data|
+#     puts "#{book_data["name"]}, published by #{book_data["publisher"]} and written by #{book_data["authors"][0]}"
+#   end
+# end
+#
+# def show_character_tv_series(name)
+#   series_array = get_character_by_name_from_api(name)
+#
+#   puts "-------------------------------"
+#   print_name(series_array)
+#   # print_series(series_array)
+#   # print_played_by(series_array)
+#   puts "-------------------------------"
+# end
+#
+# ## BONUS
+#
+# # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
+# # can you split it up into helper methods?
+
+
+
+
+# =============================================================================
+
+
 require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_by_name_from_api(name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  # response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{char_name}")
+  response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{name.gsub!(" ", "%20")}")
+  # response_string = response_string_before.gsub!(" ", "%20")
+  response_array = JSON.parse(response_string)
+  response_hash = response_array[0]
+  return response_hash
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def inform_user_about_options(hash)
+puts "What info are you interested in?"
+puts "You can ask about:"
+puts hash.keys
 end
 
-def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+
+def get_info_user_interested_in
+  input= gets.chomp
+end
+
+
+def user_requested(response_hash,info)
+  puts "Here's your info: "
+  puts (response_hash[info])
+end
+
+
+
+def show_character_info(name)
+  series_array = get_character_by_name_from_api(name)
+  puts "========================"
+  inform_user_about_options(series_array)
+  puts "========================"
+  info=get_info_user_interested_in
+  puts "========================"
+  user_requested(series_array,info)
 end
 
 ## BONUS
