5e99e2dc683be39eb6ee92a35078f02602fca234
Ed Putans <edputans@gmail.com>
2018-09-18 16:02:07 +0100

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,15 +4,17 @@ require 'pry'
 
 def get_character_by_name_from_api(name)
   #make the web request
-  # response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{char_name}")
   response_string_before = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{name}")
   response_string = response_string_before.gsub!(" ", "%20")
+  #this checks the url for the name and replaces the spaces with "%20"
   response_array = JSON.parse(response_string)
   response_hash = response_array[0]
-  return response_hash
+  response_hash
+  #returns the hash we are interested in
 end
 
 def inform_user_about_options(hash)
+#lists the user's options
 puts "What info are you interested in?"
 puts "You can ask about:"
 puts hash.keys
@@ -20,11 +22,13 @@ end
 
 
 def get_info_user_interested_in
+  #receives the info user is interested in
   input= gets.chomp.downcase
 end
 
 
 def user_requested(response_hash,info)
+  #finds the info user wants in the array and prints it
   puts "Here's your info: "
   puts (response_hash[info])
 end
@@ -32,6 +36,7 @@ end
 
 
 def show_character_info(name)
+  #calls all the aforementioned methods
   series_array = get_character_by_name_from_api(name)
   puts "========================"
   inform_user_about_options(series_array)
