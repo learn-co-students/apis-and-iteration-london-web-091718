b20df04c98b76af7d2036ba996fb8da8f83434b4
OllyDS <desousa88@gmail.com>
2018-09-18 18:45:10 +0100

    group lab completed

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
 def welcome
-  # puts out a welcome message here!
+  puts "Good Morrow, mine eyes doth taketh interest in thee!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  print "Enter a name for the Many-Faced God: "
+  gets.chomp
 end
