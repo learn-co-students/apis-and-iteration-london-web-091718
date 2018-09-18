ee004e566fed2ad1ffec18a116ed6ede3dbb9f0f
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-18 16:35:07 +0100

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,33 +2,78 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_books_from_api(character)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get('https://anapioficeandfire.com/api/characters/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+ #get array of books require_relative
+  books_url = []
+  response_hash.each do |got_character|
+    if got_character["name"].downcase == character
+      books_url << got_character["books"]
+    end
+  end
+  books_url
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def print_books(books_url)
+  book_titles = []
+  books_url.flatten.each do |url|
+    response_book = RestClient.get(url)
+    book_hash = JSON.parse(response_book)
+    book_titles << book_hash["name"]
+  end
+  puts book_titles
 end
 
-def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+def show_character_books(character)
+  books_url = get_character_books_from_api(character)
+  print_books(books_url)
+end
+
+
+#-------------------------------------------------
+
+
+
+
+def get_characters_from_book_api(book)
+  response_books = RestClient.get('https://anapioficeandfire.com/api/books/')
+  book_hash = JSON.parse(response_books)
+  #create array of characters url
+  characters_url = []
+  book_hash.each do |got_books|
+    #binding.pry
+    if got_books["name"].downcase == book
+    characters_url << got_books["characters"]
+    end
+  end
+  characters_url
+end
+
+
+def print_characters(characters_url)
+  character_names = []
+  characters_url.flatten.each do |url|
+    #binding.pry
+    response_character = RestClient.get(url)
+    character_hash = JSON.parse(response_character)
+    if character_hash["name"] == ""
+      character_names << character_hash["aliases"]
+    elsif character_hash["name"] != ""
+      character_names << character_hash["name"]
+    end
+  end
+  puts character_names
+
+end
+
+def show_character_in_a_book (book)
+  # ask user for name of book
+  # return a list of all books associated with the character
+  characters_url = get_characters_from_book_api(book)
+  print_characters(characters_url)
 end
 
 ## BONUS
