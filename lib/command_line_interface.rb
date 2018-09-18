75841f0aeb73d001bc70dbfaa91a87166f3cefb6
deedeeh <dinahafez.8@gmail.com>
2018-09-18 15:46:53 +0100

    Add api group work

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,13 @@
+require_relative 'api_communicator.rb'
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to Game of Thrones books collection!"
 end
 
-def get_character_from_user
-  puts "please enter a character"
+def get_book_from_user
+  get_books
+  puts "please enter a book name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp.downcase
 end
