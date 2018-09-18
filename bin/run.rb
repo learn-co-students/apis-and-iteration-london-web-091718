fe409687d5cea69b16831b58085e3e1615925311
Ed Putans <edputans@gmail.com>
2018-09-18 16:21:10 +0100

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,5 +3,5 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-name= get_name_from_user
+name = get_name_from_user
 show_character_info(name)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,17 +4,14 @@ require 'pry'
 
 def get_character_by_name_from_api(name)
   #make the web request
-  response_string_before = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{name}")
-  response_string = response_string_before.gsub!(" ", "%20")
-  #this checks the url for the name and replaces the spaces with "%20"
+  # response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{char_name}")
+  response_string = RestClient.get("https://anapioficeandfire.com/api/characters/?name=#{name.gsub!(' ', '%20')}")
   response_array = JSON.parse(response_string)
   response_hash = response_array[0]
-  response_hash
-  #returns the hash we are interested in
+  return response_hash
 end
 
 def inform_user_about_options(hash)
-#lists the user's options
 puts "What info are you interested in?"
 puts "You can ask about:"
 puts hash.keys
@@ -22,13 +19,11 @@ end
 
 
 def get_info_user_interested_in
-  #receives the info user is interested in
-  input= gets.chomp.downcase
+  input= gets.chomp
 end
 
 
 def user_requested(response_hash,info)
-  #finds the info user wants in the array and prints it
   puts "Here's your info: "
   puts (response_hash[info])
 end
@@ -36,11 +31,9 @@ end
 
 
 def show_character_info(name)
-  #calls all the aforementioned methods
   series_array = get_character_by_name_from_api(name)
   puts "========================"
   inform_user_about_options(series_array)
-  puts "========================"
   info=get_info_user_interested_in
   puts "========================"
   user_requested(series_array,info)
