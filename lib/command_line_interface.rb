61aee0b1545b3ec54eeddfeaeb2c6802cb724197
Ed Putans <edputans@gmail.com>
2018-09-18 16:00:22 +0100

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
-  # puts out a welcome message here!
+ # puts out a welcome message here!
+ puts "Welcome to the Game of Thrones API."
 end
 
-def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+def get_name_from_user
+ puts "Please enter a character name"
+ # use gets to capture the user's input. This method should return that input, downcased.
+ gets.chomp.downcase
 end
