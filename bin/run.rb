f8bcf67de84e73e2c8c195bc5b7adcddbd856942
github username <github email address>
2019-05-29 16:51:30 +0100

    Inital

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,8 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+
+loop do
+    character = get_character_from_user
+    show_character_movies(character)
+end
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,35 +3,31 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  response_hash['results'].detect{|result| result['name'].downcase === character}
 end
 
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+    films = films_hash['films'].each do |film|
+      response_string = RestClient.get(film)
+      response_hash = JSON.parse(response_string)
+      puts ""
+      puts "###############################################"
+      puts ""
+      puts "Title: #{response_hash['title']}"
+      puts "Director: #{response_hash['director']}"
+      puts "Release Date: #{response_hash['release_date']}"
+    end
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+  if films_array
+    print_movies(films_array)
+  else
+    puts ''
+    puts 'Character not found :( '
+    puts ''
+  end
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
